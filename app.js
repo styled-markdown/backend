23712396df0c4e16cf6f1eb748472388a54a4703
Seoji Kang <lazymoledg@gmail.com>
2022-02-26 03:41:43 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json("error");
+});
+
+module.exports = app;
