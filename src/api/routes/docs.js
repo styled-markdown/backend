1ae3e4b103b81eeebf60ca30f01b1f3ee4f8b209
Seoji Kang <lazymoledg@gmail.com>
2022-03-09 05:59:10 +0900

    [17] API - GET 문서 정보
    - Feat: GET 문서 정보 API 추가
    - Feat: req.params ObjectId validator 추가

--- src/api/routes/docs.js
+++ src/api/routes/docs.js
@@ -5,5 +5,11 @@ const validator = require("../middlewares/validator");
 const docsController = require("../controllers/docs.controllers");
 
 router.post("/", validator.verifyAccessToken, docsController.createDoc);
+router.get(
+  "/:id",
+  validator.verifyAccessToken,
+  validator.verifyParams,
+  docsController.getDetail
+);
 
 module.exports = router;
